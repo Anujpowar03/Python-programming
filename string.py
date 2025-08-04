7d0a2ef714d0f83647d9845867d2c9d75be08c07
Anujpowar03 <anujpowar03@gmail.com>
2025-08-04 15:56:37 +0530

    Codes

---
+++ string.py
@@ -0,0 +1,12 @@
+str1 = 'Hello, World!'
+print(str1)
+
+str2 = "Python is fun."
+print(str2)
+
+str3 = '''Let's learn Python.'''
+print(str3)
+
+str4 = """This is a multi-line string.
+It can span multiple lines."""
+print(str4)
